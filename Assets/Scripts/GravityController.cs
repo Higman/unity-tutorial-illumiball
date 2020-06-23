624691fa41fac47d4064302ae55f1f6b0a5a2cb9
Higman <s19g451@stu.kagawa-u.ac.jp>
2020-06-24 02:11:21 +0900

    finish tutorial

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+
+    const float Gravity = 9.8f;
+
+    public float gravityScale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = new Vector3();
+
+        if (Application.isEditor) {
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
+
+            if (Input.GetKey("z")) {
+                vector.y = 1.0f;
+            } else {
+                vector.y = -1.0f;
+            }
+        } else {
+            vector = Input.acceleration;
+        }
+
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+    }
+}
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool fallIn;
+
+    public bool IsFallIn()
+    {
+        return fallIn;
+    }
+
+    public string activeTag;
+
+    void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        if (other.gameObject.tag == activeTag)
+        {
+            r.velocity *= 0.9f;
+            r.AddForce(direction * r.mass * 20f);
+        } else
+        {
+            r.AddForce(-direction * r.mass * 80f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = false;
+        }
+    }
+}
